cf6e30f71b559906c2cb79fe375385523dbeaed2
龙缘科技神林 <773933146@qq.com>
2019-06-17 16:39:14 +0800

    Update ReEnv.py

--- ReEnv.py
+++ ReEnv.py
@@ -1,2 +1,2 @@
 import win32api,win32gui,win32con
-win32api.SendMessage(win32con.HWND_BROADCAST,win32con.WM_SETTINGCHANGE,0,"Environment");
+win32api.SendMessage(win32con.HWND_BROADCAST,win32con.WM_SETTINGCHANGE,0,"ENVIRONMENT");
